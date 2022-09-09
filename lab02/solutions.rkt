aa4d250c456373f6345780145922ac8c87d35431
Artem Murashko <Artem.Murashko@kaspersky.com>
2022-09-09 21:29:47 +0300

    solve 1b

--- lab02/solutions.rkt
+++ lab02/solutions.rkt
@@ -1,1 +1,19 @@
 #lang slideshow
+
+; 1(b)
+
+(define (count-zeros lst)
+  (define (skip-leading-zeros lst)
+    (cond
+      [(empty? lst) lst]
+      [(= (first lst) 1) lst]
+      [else (skip-leading-zeros(rest lst))]))
+
+  (define (count-all-zeros lst)
+    (cond
+      [(empty? lst) 0]
+      [(= (first lst) 1) (count-all-zeros(rest lst))]
+      [else (+ 1
+               (count-all-zeros(rest lst)))]))
+      
+  (count-all-zeros (skip-leading-zeros lst)))
